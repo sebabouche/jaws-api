2a25ab9f89f0b95fba252477453cac553e30449c
sebabouche <s.nicolaidis@me.com>
2016-06-10 11:05:08 +0200

    Devise installation

--- Gemfile
+++ Gemfile
@@ -12,7 +12,9 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
-gem 'devise_token_auth'
+gem 'devise'
+gem 'jwt'
+
 gem 'omniauth-facebook'
 gem 'rack-cors', :require => 'rack/cors'
 gem 'fuzz_ball'
--- db/schema.rb
+++ db/schema.rb
@@ -26,37 +26,6 @@ ActiveRecord::Schema.define(version: 20160604212209) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "mangs", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-    t.string   "email"
-    t.string   "encrypted_password",          default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.string   "reset_password_redirect_url"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",               default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "confirm_success_url"
-    t.string   "unconfirmed_email"
-    t.string   "name"
-    t.string   "nickname"
-    t.string   "image"
-    t.string   "provider"
-    t.string   "uid",                         default: "", null: false
-    t.text     "tokens"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "favorite_color"
-    t.index ["email"], name: "index_mangs_on_email", using: :btree
-    t.index ["reset_password_token"], name: "index_mangs_on_reset_password_token", unique: true, using: :btree
-    t.index ["uid"], name: "index_mangs_on_uid", unique: true, using: :btree
-  end
-
   create_table "merchants", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
     t.string   "name"
     t.string   "img"
@@ -78,37 +47,6 @@ ActiveRecord::Schema.define(version: 20160604212209) do
     t.integer  "people"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-  end
-
-  create_table "users", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-    t.string   "email"
-    t.string   "encrypted_password",          default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.string   "reset_password_redirect_url"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",               default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "confirm_success_url"
-    t.string   "unconfirmed_email"
-    t.string   "name"
-    t.string   "nickname"
-    t.string   "image"
-    t.string   "provider"
-    t.string   "uid",                         default: "", null: false
-    t.text     "tokens"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "favorite_color"
-    t.index ["email"], name: "index_users_on_email", using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-    t.index ["uid"], name: "index_users_on_uid", unique: true, using: :btree
   end
 
 end
